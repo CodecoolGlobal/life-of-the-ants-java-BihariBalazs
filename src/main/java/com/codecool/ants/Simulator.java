88f853a6e5541ad14f5b449477ee3eab9826fd60
BihariBalazs <bihari.b16@gmail.com>
2021-11-09 17:07:11 +0100

    ADD: implement simulator

--- src/main/java/com/codecool/ants/Simulator.java
+++ src/main/java/com/codecool/ants/Simulator.java
@@ -1,9 +1,16 @@
 package com.codecool.ants;
 
+import com.codecool.ants.ants.Colony;
+
 public class  Simulator {
+    private static final int ROUNDS = 10;
 
     public static void main(String[] args) {
-        System.out.println("You can use `mvn package; java -jar target/ants-1.0.jar` to run this app from the console");
+        Colony colony = new Colony(100);
+        colony.createAnts(5, 4, 2);
+        for (int time = 0; time < ROUNDS; time++){
+            System.out.println(colony);
+        }
     }
 
 }
