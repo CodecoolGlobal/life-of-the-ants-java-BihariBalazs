429c39cb901978263fb27b9b8e332a739481513a
BihariBalazs <bihari.b16@gmail.com>
2021-11-09 16:35:20 +0100

    ADD: create Direction constructor

--- src/main/java/com/codecool/ants/geometry/Direction.java
+++ src/main/java/com/codecool/ants/geometry/Direction.java
@@ -1,5 +1,16 @@
 package com.codecool.ants.geometry;
 
     public enum Direction {
-        NORTH, EAST, SOUTH, WEST;
+        NORTH (0, 1),
+        EAST (1, 0),
+        SOUTH (0, -1),
+        WEST (-1, 0);
+
+        private final int x;
+        private final int y;
+
+        Direction(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
