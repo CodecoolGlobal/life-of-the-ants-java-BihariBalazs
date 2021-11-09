5463d4c104f5648373ec2d4abceb59f9442a67b5
BihariBalazs <bihari.b16@gmail.com>
2021-11-09 18:10:22 +0100

    ADD: implement direction to other ant method

--- src/main/java/com/codecool/ants/ants/Ant.java
+++ src/main/java/com/codecool/ants/ants/Ant.java
@@ -3,17 +3,29 @@ package com.codecool.ants.ants;
 import com.codecool.ants.geometry.Direction;
 import com.codecool.ants.geometry.Position;
 
+import java.util.Random;
+
 
 public abstract class Ant {
-        private Position position;
+    static Random random = new Random();
+
+    private Position position;
 
     public Ant(int x, int y) {
         this.position = new Position(x, y);
     }
 
-    public void move(Direction direction){
+    public void move(Direction direction) {
         this.position.setX(position.getX() + direction.getX());
         this.position.setY(position.getY() + direction.getY());
+    }
+
+    public Direction directionTo(Ant otherAnt){
+        if (Math.abs(position.getX() - otherAnt.position.getX()) > Math.abs(position.getY() - otherAnt.position.getY())){
+           return position.getX() > otherAnt.position.getX() ? Direction.WEST : Direction.EAST;
+        } else {
+            return position.getY() > otherAnt.position.getY() ? Direction.NORTH : Direction.SOUTH;
+        }
     }
 
     public abstract void step();
