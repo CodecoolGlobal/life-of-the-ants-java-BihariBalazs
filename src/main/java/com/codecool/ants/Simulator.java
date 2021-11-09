b0eeba7faaa90daa2443108c31985b002012686e
BihariBalazs <bihari.b16@gmail.com>
2021-11-09 17:38:38 +0100

    ADD: create simulation

--- src/main/java/com/codecool/ants/Simulator.java
+++ src/main/java/com/codecool/ants/Simulator.java
@@ -7,7 +7,7 @@ public class  Simulator {
 
     public static void main(String[] args) {
         Colony colony = new Colony(100);
-        colony.createAnts(5, 4, 2);
+        colony.createAnts(5, 0, 0);
         for (int time = 0; time < ROUNDS; time++){
             System.out.println(colony);
             colony.step();
