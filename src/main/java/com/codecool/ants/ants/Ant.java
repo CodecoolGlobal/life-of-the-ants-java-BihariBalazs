b7f056bd79184a9390a358894450780b81e20298
BihariBalazs <bihari.b16@gmail.com>
2021-11-09 17:19:01 +0100

    ADD: implement move method

--- src/main/java/com/codecool/ants/ants/Ant.java
+++ src/main/java/com/codecool/ants/ants/Ant.java
@@ -1,13 +1,22 @@
 package com.codecool.ants.ants;
 
+import com.codecool.ants.geometry.Direction;
 import com.codecool.ants.geometry.Position;
 
 
-public class Ant {
+public abstract class Ant {
         private Position position;
 
     public Ant(int x, int y) {
         this.position.setX(x);
         this.position.setY(y);
     }
+
+    public void move(Direction direction){
+        this.position.setX(position.getX() + direction.getX());
+        this.position.setY(position.getY() + direction.getY());
+    }
+
+    public abstract void step();
+
 }
