11050fb4734d2694120bd23c78e907cf63694c5f
BihariBalazs <bihari.b16@gmail.com>
2021-11-09 17:58:39 +0100

    ADD: implement queen mating methods

--- src/main/java/com/codecool/ants/ants/Queen.java
+++ src/main/java/com/codecool/ants/ants/Queen.java
@@ -1,8 +1,24 @@
 package com.codecool.ants.ants;
 
+import java.util.Random;
+
 public class Queen extends Ant{
+    private static final int MATING_AWAIT_MIN = 50;
+    private int counterAfterMating = 0;
+
 
     public Queen(int x, int y) {
         super(x, y);
+    }
+
+    @Override
+    public void step() {
+        if (counterAfterMating > 0){
+            counterAfterMating--;
+        }
+    }
+
+    public void awaitForMating(){
+        counterAfterMating = random.nextInt(MATING_AWAIT_MIN) + MATING_AWAIT_MIN;
     }
 }
