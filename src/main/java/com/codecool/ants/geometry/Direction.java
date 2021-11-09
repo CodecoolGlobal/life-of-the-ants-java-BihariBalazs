f9f8b04ab0bf4e6fc5bab36e9ee99e4134cd4d8e
BihariBalazs <bihari.b16@gmail.com>
2021-11-09 16:40:49 +0100

    ADD: create random direction

--- src/main/java/com/codecool/ants/geometry/Direction.java
+++ src/main/java/com/codecool/ants/geometry/Direction.java
@@ -1,16 +1,24 @@
 package com.codecool.ants.geometry;
 
-    public enum Direction {
+import java.util.Random;
+
+public enum Direction {
         NORTH (0, 1),
         EAST (1, 0),
         SOUTH (0, -1),
         WEST (-1, 0);
 
+        private static Random random = new Random();
         private final int x;
         private final int y;
+        private static final int numberOfDirections = Direction.values().length;
 
         Direction(int x, int y) {
             this.x = x;
             this.y = y;
+        }
+
+        public static Direction getRandomDirection(){
+            return Direction.values()[random.nextInt(numberOfDirections)];
         }
     }
