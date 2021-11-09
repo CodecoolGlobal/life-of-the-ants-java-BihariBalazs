0bd98d4d12720d77909378af1091585aef53118b
BihariBalazs <bihari.b16@gmail.com>
2021-11-09 17:19:58 +0100

    ADD: implement worker step

--- src/main/java/com/codecool/ants/ants/Worker.java
+++ src/main/java/com/codecool/ants/ants/Worker.java
@@ -1,9 +1,14 @@
 package com.codecool.ants.ants;
 
+import com.codecool.ants.geometry.Direction;
+
 public class Worker extends Ant{
     public Worker(int x, int y) {
         super(x, y);
     }
 
-
+    @Override
+    public void step() {
+        move(Direction.getRandomDirection());
+    }
 }
