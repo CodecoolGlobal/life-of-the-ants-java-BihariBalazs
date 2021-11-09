2fc4d970aa1a74befd18a6f4df3b0a60e54cf637
BihariBalazs <bihari.b16@gmail.com>
2021-11-09 16:30:33 +0100

    ADD: create drone constructor

--- src/main/java/com/codecool/ants/ants/Drone.java
+++ src/main/java/com/codecool/ants/ants/Drone.java
@@ -1,5 +1,10 @@
 package com.codecool.ants.ants;
 
-public class Drone {
+public class Drone extends Ant{
+    private Queen queen;
 
+    public Drone(int x, int y, Queen queen) {
+        super(x, y);
+        this.queen = queen;
+    }
 }
